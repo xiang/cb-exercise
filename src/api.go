a2340a95a3906fb3d09686e04296a5c10680fca1
Sean Fortner <seanjfortner@gmail.com>
2018-09-25 12:41:39 -0300

    consume CPU

--- src/api.go
+++ src/api.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"runtime"
+	"runtime/debug"
 	"time"
 )
 
@@ -15,7 +17,7 @@ type response struct {
 	Mem  string
 }
 
-func load() {
+func eatMem() {
 	a := make([]int64, 1024*1024*32)
 	for i := range a {
 		a[i] = rand.Int63()
@@ -23,8 +25,23 @@ func load() {
 	time.Sleep(time.Second * 4)
 	a = nil
 	runtime.GC()
-	//	debug.FreeOSMemory()
+	debug.FreeOSMemory()
 	return
+}
+
+func eatCPU() {
+	go func() {
+		f, err := os.Open(os.DevNull)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		for i := 0; i < 1000000; i++ {
+			fmt.Fprintf(f, "asdf")
+		}
+		time.Sleep(10 * time.Second)
+	}()
 }
 
 func main() {
@@ -32,15 +49,18 @@ func main() {
 		currentTime := time.Now().Format("3:04PM GMT-07 on Jan 02 2006")
 		var m runtime.MemStats
 		runtime.ReadMemStats(&m)
-		memUsage := fmt.Sprintf("Alloc = %v MB, Sys = %v MB", m.Alloc/1024/1024, m.Sys/1024/1024)
+		memAlloc := m.Alloc / 1024 / 1024
+		memSys := m.Sys / 1024 / 1024
+		memUsage := fmt.Sprintf("Alloc = %v MB, Sys = %v MB", memAlloc, memSys)
 		res, _ := json.Marshal(
 			&response{
 				Time: currentTime,
 				Mem:  memUsage,
 			})
 		fmt.Fprintf(w, "%s", res)
-		fmt.Fprintf(w, "\n%d", runtime.NumGoroutine())
-		go load()
+		//		fmt.Fprintf(w, "\n%d", runtime.NumGoroutine())
+		//go eatMem()
+		eatCPU()
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
