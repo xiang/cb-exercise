cc8a4e76568f242b8418c3c7a4af3187969c5eba
Sean Fortner <seanjfortner@gmail.com>
2018-09-25 23:24:07 -0300

    bootstrap fixes

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 echo StrictHostKeyChecking no > ~/.ssh/config
 git clone https://github.com/xiang/cb-exercise.git
 cd cb-exercise/src
 sudo docker build -t api .
-sudo docker run -it --rm -p 8080:8080 api
+sudo docker run --rm -p 8080:8080 api
